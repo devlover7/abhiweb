311d2ece6135cb3ffa02720794eb76bfaa23509a
abhinav <gour.abhinav@orangemantra.in>
2020-05-10 21:09:42 +0530

    Updates

---
+++ precache-manifest.b8bb70490bdff55a61ee064244cb231b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f79010ceedeb9c4970beff4e2f53fc34",
+    "url": "/abhiweb/index.html"
+  },
+  {
+    "revision": "abb77f8a040480769d27",
+    "url": "/abhiweb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0d51502e7e7a93f02f6a",
+    "url": "/abhiweb/static/js/2.6e2f46ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/abhiweb/static/js/2.6e2f46ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abb77f8a040480769d27",
+    "url": "/abhiweb/static/js/main.db03beab.chunk.js"
+  },
+  {
+    "revision": "e0b5c8d3cd9c949b4a04",
+    "url": "/abhiweb/static/js/runtime-main.1d2b99e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/abhiweb/static/media/logo.5d5d9eef.svg"
+  }
+]);
